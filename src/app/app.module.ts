82bd5d011755308bf485e89c38b44dc7542e41e2
Ed Ezekiel <ed@ubiquisoft.com>
2021-01-14 16:36:07 -0600

    Use two-way data binding with an input element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './components/server/server.component';
 import { ServersComponent } from './components/servers/servers.component';
@@ -17,6 +17,7 @@ import { SuccessComponent } from './success/success.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
