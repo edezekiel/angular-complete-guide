80f1124ab85e1e2af5baf030fca2494622729bc4
Ed Ezekiel <ed@ubiquisoft.com>
2021-01-14 16:24:19 -0600

    Using the click property binding

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  
+  serverCreationStatus = 'No server was created';
+
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true
@@ -17,4 +18,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
