b64e05b9087ea7724f420aaa0493850e01c2e663
Ed Ezekiel <ed@ubiquisoft.com>
2021-01-15 09:23:33 -0600

    Use ngStyle to set attribute style, use property binding to configure it

--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
   serverId: number = 10;
-  serverStatus: string = 'online';
+  serverStatus: string = 'offline';
 
-  constructor() { }
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit(): void {
   }
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
