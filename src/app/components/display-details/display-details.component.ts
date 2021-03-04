2c14fa7ef7a6047aa830151cab4710658d650e7e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-03-04 11:11:19 -0600

    Test Lifecycle properties

--- src/app/components/display-details/display-details.component.ts
+++ src/app/components/display-details/display-details.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display-details',
   templateUrl: './display-details.component.html',
   styleUrls: ['./display-details.component.css']
 })
-export class DisplayDetailsComponent implements OnInit {
+export class DisplayDetailsComponent implements OnInit, OnChanges {
   showDetails: boolean = false;
   clickTimeStamps: Array<any> = [];
 
   constructor() { }
 
+  ngOnChanges(changes) {
+    console.log('ngOnChanges Called, changes = ', changes)
+  }
+
   ngOnInit(): void {
+    console.log('ngOnInit called');
   }
 
   toggleDetails(e: any) {
