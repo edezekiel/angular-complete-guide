78f3a30ed8806a8f0cfad717988893d2e8a5773d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-03-29 11:46:15 -0500

    Use HostBinding to bind to elRef.style.backgroundColor

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,16 +1,20 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) { 
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightBlue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightBlue');
+    this.backgroundColor = 'lightBlue';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
